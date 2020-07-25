797eba942e6aa4afb109bd336c32fe1caa1d80dc
Nilsonlinux <nilsonlinux@gmail.com>
2020-07-25 17:39:15 -0300

    Add files via upload

--- npdvs.sh
+++ npdvs.sh
@@ -10,7 +10,6 @@ i#!/bin/bash
 # y=yellow bu=blue m=magenta c=cyan w=white
 # endc=end-color end=end-argument
 pdvs_ips='139 131 122 123 124 25 102 103 104 105 107 120 140 133 110 11 112 113 114 130 116 55 59 117 225 132 138 128' #FINAL dos IPS DOS PDVS...
-gt="100"
 version="2.7"
 GMCORE='6.36'
 IPSERV='192.168'
