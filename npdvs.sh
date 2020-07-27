a98947e5f2aa0756be0e478da08c8ed38c46ef88
Nilsonlinux <nilsonlinux@gmail.com>
2020-07-27 20:28:49 -0300

    Add files via upload

--- npdvs.sh
+++ npdvs.sh
@@ -4,23 +4,15 @@
 # Automatização sobre demanda
 # Nilsonlinux 23/07/2020
 # Colabore com o projeto
+# https://github.com/nilsonlinux/npdvs
 # -------------------------------------------------------
-# Variables
-# b=bold u=underline bl=black r=red g=green
-# y=yellow bu=blue m=magenta c=cyan w=white
-# endc=end-color end=end-argument
+#Variáveis
 pdvs_ips='139 131 122 123 124 25 102 103 104 105 107 120 140 133 110 11 112 113 114 130 116 55 59 117 225 132 138 128' #FINAL dos IPS DOS PDVS...
 GW="100"
 version="2.9"
 GMCORE='6.36'
-MGVSERV="6.45"
-MGVUSER="leandro"
-MGVPASS="Mgv62019"
 IPSERV='192.168'
 RES="1920x1030"
-GUSER="maxpos_gw"
-GPASS="terminal"
-IMPORTA="pdvmaxipos.mateus"
 spath="$( cd "$( dirname $0 )" && pwd )"
 a='\033[1;33m'       # Amarelo
 p='\033[0;35m'       # Purple
@@ -155,17 +147,15 @@ logoNPDVs () {
           ${y}Versão :${end}${bu} ${version}${end}"
     echo
 }
-# NPDVs
-# Exit CliPDVs
+# Exit NPDVss
 NPDVsExit () {
   logoNPDVs 
-  echo -e " Obrigado por utilizar o ${b}CliPDVs${end}
+  echo -e " Obrigado por utilizar o ${b}NPDVs${end}
  Desenvolvido por: Nilsonlinux
  ${b}Telegram -➤ ${bu}https://t.me/nilsonlinux${end}"
   echo && sleep 1
   exit
 }
-
 # APT Update
 #aptgupd () {
 #  echo && echo -e " ${y}Preparing To Perform APT Update${endc}"
@@ -188,7 +178,7 @@ NPDVsExit () {
 checkinternet () {
   if ping -c 1 google.com &>/dev/null; then
     echo -e " Checando conexão com a internet: ${vr}CONECTADO ✅${endc}"
-    CLiCheck
+    NPDVsCheck
   else
     echo -e " Checando conexão com a internet: ${r}DESCONECTADO ❌${endc}
  ${y}Você precisa está conectado para a utilização do CliPDVs${endc}"
@@ -205,13 +195,13 @@ NPDVsStart () {
 # New Version Check & Update
 cliupdate () {
   logoNPDVs
-  echo -e " Preparando atualização ${b}CliPDVs${end}"
+  echo -e " Preparando atualização ${b}NPDVs${end}"
   echo && echo -en " ${y}Precione ENTER para continuar${endc}"
   read input
-  echo && echo -e " Atualizando ${b}CliPDVs${end}, Por favor aguarde..."
-  wget https://raw.githubusercontent.com/sistemaclipdvs/npdvs/master/npdvs.sh -O $spath/npdvs.sh &>/dev/null
-  sleep 1 && echo -e " ${b}CliPDVs${end} Atualização aplicada com sucesso "
-  sleep 1 && echo -e " Restartando ${b}CliPDVs${end}..."
+  echo && echo -e " Atualizando ${b}NPDVs${end}, Por favor aguarde..."
+  wget https://raw.githubusercontent.com/nilsonlinux/npdvs/master/npdvs.sh -O $spath/npdvs.sh &>/dev/null
+  sleep 1 && echo -e " ${b}NPDVs${end} Atualização aplicada com sucesso "
+  sleep 1 && echo -e " Restartando ${b}NPDVs${end}..."
   sleep 2
   NPDVsStart
 }
@@ -227,17 +217,17 @@ CLiCheck () {
     read option
     case $option in
       s) cliupdate ;;
-      n) echo -e " ${y}Ok, Iniciando CliPDVs.${endc}"; sleep 1; aptgupd ;;
-      *) echo " \"$option\" Opção inválida, tente outra opção."; sleep 1; CLiCheck ;;
+      n) echo -e " ${y}Ok, Iniciando NPDVs.${endc}"; sleep 1; aptgupd ;;
+      *) echo " \"$option\" Opção inválida, tente outra opção."; sleep 1; NPDVsCheck ;;
     esac
   else
-    echo -e " Checando novas atualizações: ${g}CliPDVs está atualizado${endc}"
+    echo -e " Checando novas atualizações: ${g}NPDVs está atualizado${endc}"
     aptgupd
   fi
 }
 ##################
 # INICIALIZAÇÃO DO SCRIPT
-logoNPDVs && echo -e " ${y}Inicializando CliPDVs...${endc}" && checkinternet
+logoNPDVs && echo -e " ${y}Inicializando NPDVs...${endc}" && checkinternet
 # -------------------------------------------------------
 
 # (7-2) Install Mozilla Firefox
@@ -275,7 +265,7 @@ clear
 ##########
   clear
 logoNPDVs
-echo -e " ${r}REINICIALIZAÇÃO DOS TERMINAIS (CliPDVs) ${end}"
+echo -e " ${r}REINICIALIZAÇÃO DOS TERMINAIS (NPDVs) ${end}"
 loja
 echo -e "DIGITE O ${y}FINAL DO IP${end} ${r}QUE DESEJA REINICIAR: ${end}"
 read -p "$IPSERV.$FX." $read IP
@@ -319,7 +309,7 @@ fi
 # (1) Atualizar PDVs
 atualizar_pdvs () {
   logoNPDVs
-echo -e " ${c}ATUALIZAÇÃO DOS TERMINAIS (CliPDVs)${end}"
+echo -e " ${c}ATUALIZAÇÃO DOS TERMINAIS (NPDVs)${end}"
 painel
 echo -e "DIGITE A ${c}FAIXA${end} ${br}REFERÊNTE A SUA FILIAL: ${end}"
 read -p "$IPSERV." $read FX
@@ -327,7 +317,7 @@ clear
 ##########
   clear
 logoNPDVs
-echo -e " ${c}ATUALIZAÇÃO DOS TERMINAIS (CliPDVs) ${end}"
+echo -e " ${c}ATUALIZAÇÃO DOS TERMINAIS (NPDVs) ${end}"
 loja
 echo -e "DIGITE O ${c}FINAL DO IP${end} ${br}QUE DESEJA ATUALIZAR: ${end}"
 read -p "$IPSERV.$FX." $read IP
@@ -371,7 +361,7 @@ fi
 wait
 reiniciar_todos () {
 logoNPDVs
-echo -e " ${v}REINICIALIZAÇÃO DOS TERMINAIS (CliPDVs)"
+echo -e " ${v}REINICIALIZAÇÃO DOS TERMINAIS (NPDVs)"
 echo -e "${v}---------------------------------------------------${end}
   ${br}Reinicialização dos terminais 
   por faixa.${end}
@@ -405,7 +395,7 @@ read input
 # (1) Desligar PDVs
 desligar_pdvs () {
   logoNPDVs
-echo -e " ${r}🚨 DESLIGAMENTO DOS TERMINAIS (CliPDVs) 🚨 ${end}"
+echo -e " ${r}🚨 DESLIGAMENTO DOS TERMINAIS (NPDVs) 🚨 ${end}"
 painel
 echo -e "DIGITE A ${y}FAIXA${end} ${r}REFERÊNTE A SUA FILIAL: ${end}"
 read -p "$IPSERV." $read FX
@@ -458,7 +448,7 @@ fi
 wait
 atualizar_todos () {
   logoNPDVs
-echo -e " ${bu}ATUALIZAÇÃO DOS TERMINAIS (CliPDVs)"
+echo -e " ${bu}ATUALIZAÇÃO DOS TERMINAIS (NPDVs)"
 echo -e "${bu}---------------------------------------------------${end}
   ${br}Atualização dos terminais 
   por faixa de sua filial${end}
@@ -494,7 +484,7 @@ wait
 desligar_todos () {
   logoNPDVs
 echo -e "${v}---------------------------------------------------${end}"
-echo -e "${r}🚨 DESLIGAMENTO DOS TERMINAIS (CliPDVs) 🚨"
+echo -e "${r}🚨 DESLIGAMENTO DOS TERMINAIS (NPDVs) 🚨"
 echo -e "${v}--------------------------------------------------- ${end}"
 echo -e " DIGITE A${y} FAIXA DA FILIAL${end} ${v}QUE DESEJA DESLIGAR: ${end}"
 echo -e " Caso queira desistir${y}, apenas deixe o campo em branco e dê enter... ${end}"
@@ -524,7 +514,7 @@ read input
 # (1) Atualizar PDVs
 atualizar_imagem () {
   logoNPDVs
-echo -e " ${c}ATUALIZAÇÃO DE IMAGEM DOS TERMINAIS (CliPDVs)"
+echo -e " ${c}ATUALIZAÇÃO DE IMAGEM DOS TERMINAIS (NPDVs)"
 painel
 echo -e "DIGITE A ${c}FAIXA${end} ${br}REFERÊNTE A SUA FILIAL: ${end}"
 read -p "$IPSERV." $read FX
@@ -532,7 +522,7 @@ clear
 ##########
   clear
   logoNPDVs
-echo -e " ${c}ATUALIZAÇÃO DE IMAGEM DOS TERMINAIS (CliPDVs)"
+echo -e " ${c}ATUALIZAÇÃO DE IMAGEM DOS TERMINAIS (NPDVs)"
 loja
 echo -e "DIGITE O ${c}FINAL DO IP${end} ${br}QUE DESEJA ATUALIZAR IMAGEM: ${end}"
 read -p "$IPSERV.$FX." $read IP
@@ -576,7 +566,7 @@ fi
 wait
 atualizar_imagem_todos () {
   logoNPDVs
-echo -e " ${bu}ATUALIZAÇÃO DE IMAGEM DOS TERMINAIS (CliPDVs)"
+echo -e " ${bu}ATUALIZAÇÃO DE IMAGEM DOS TERMINAIS (NPDVs)"
 echo -e "${bu}---------------------------------------------------${end}
   ${br}Atualização dos terminais 
   por faixa de sua filial${end}
@@ -609,7 +599,7 @@ read input
 ################################### (11) Reiniciar Maxipos PDVs ######################################
 reiniciar_maxipos () {
   logoNPDVs
-echo -e " ${r}REINICIALIZAÇÃO DO MAXIPOS (CliPDVs)"
+echo -e " ${r}REINICIALIZAÇÃO DO MAXIPOS (NPDVs)"
 painel
 echo -e "DIGITE A ${y}FAIXA${end} ${r}REFERÊNTE A SUA FILIAL: ${end}"
 read -p "$IPSERV." $read FX
@@ -617,7 +607,7 @@ clear
 ##########
   clear
 logoNPDVs
-echo -e " ${r}REINICIALIZAÇÃO DO MAXIPOS (CliPDVs)"
+echo -e " ${r}REINICIALIZAÇÃO DO MAXIPOS (NPDVs)"
 loja
 echo -e "DIGITE O ${y}FINAL DO IP${end} ${r}QUE DESEJA REINICIAR O MAXIPOS: ${end}"
 read -p "$IPSERV.$FX." $read IP
@@ -661,7 +651,7 @@ fi
 # (5) Teste de ping PDVs
 ping_test () {
   logoNPDVs
-echo -e " ${bu}🌐 TESTE DE CONEXÕES (CliPDVs) 🌐
+echo -e " ${bu}🌐 TESTE DE CONEXÕES (NPDVs) 🌐
 ---------------------------------------------------${end}
   ${br}Faça o teste de conexão de um determinado IP
   com faixa + final de seu endereçamento...${end}
@@ -671,7 +661,7 @@ read -p "$IPSERV." $read FX
 ##########
 clear
 logoNPDVs
-echo -e " ${bu}🌐 TESTE DE CONEXÕES (CliPDVs) 🌐
+echo -e " ${bu}🌐 TESTE DE CONEXÕES (NPDVs) 🌐
 ---------------------------------------------------${end}
   ${br}Faça o teste de conexão de um determinado IP
   com faixa + final de seu endereçamento...${end}
@@ -715,7 +705,7 @@ fi
 # (5) Teste de ping link ou IP
 ping_test_ip_link () {
   logoNPDVs
-echo -e " ${bu}🌐 TESTE DE CONEXÕES (CliPDVs) 🌐
+echo -e " ${bu}🌐 TESTE DE CONEXÕES (NPDVs) 🌐
 ---------------------------------------------------${end}
 ${br}Faça o teste de conexão de um determinado IP...${end}
 ${bu}--------------------------------------------------- ${end}"
@@ -756,7 +746,7 @@ fi
 # (1) GMCORE
 gmcore () {
   logoNPDVs
-echo -e " ${vr}ACESSO GMCORE (CliPDVs)
+echo -e " ${vr}ACESSO GMCORE (NPDVs)
 ---------------------------------------------------${end}
   ${br}Acesso ao gmcore por faixa.${end}
 ${vr}--------------------------------------------------- ${end}"
@@ -795,143 +785,10 @@ echo -e "${y}Retornando para o menu principal.
 sleep 5
 fi
 }
-# --------------
-# (14) MGV-SERVER
-mgv_server () {
-  logoNPDVs
-echo -e " ${vr}ACESSO MGV SERVER (CliPDVs)
----------------------------------------------------${end}
-  ${br}Acesso ao servidor MGV.${end}
-${vr}--------------------------------------------------- ${end}"
-echo -e "${y}⌛Aguarde enquanto testamos conexão com o servidor ⌛${end}"
-sleep 1
-if ! ping -c 1 ${IPSERV}.${MGVSERV} >> /dev/null ; then
-clear
-echo -e "$v======================================= $end"
-echo -e "$v       SERVIDOR DESCONECTADO.           $end"
-echo -e "$v======================================= $end"
-echo -e "$v      _____ ____  ____   ___    _       $end"
-echo -e "$v     | ____|  _ \|  _ \ / _ \  | |      $end"
-echo -e "$v     |  _| | |_) | |_) | | | | | |      $end"
-echo -e "$v     | |___|  _ <|  _ <| |_| | |_|      $end"
-echo -e "$v     |_____|_| \_\_| \_\\____/  (_)     $end"
-echo && echo -e "$v======================================= $end"
-echo -e "$v======[ $br Status da requisição $ec $v]======= $end"
-echo -e "$a IP $end-$bu ${IPSERV}.${MGVSERV} $end- $v Sem conexão ✗$end" 
-echo -e "$v======================================= $end"
-echo -en "${y}Precione enter para retornar para o manu.${endc}"
-read input
-echo -e "$v=======================================$end" 
-else
-clear
-echo -e "$vr======================================== $end"
-echo -e "$vr         SERVIDOR CONECTADO.  $end "
-echo -e "$vr======================================== $end"
-rdesktop -g ${RES} -u ${MGVUSER} -p ${MGVPASS} ${IPSERV}.${MGVSERV}
-echo -e "$vr=======[ $br Status da requisição $ec $vr]=======$end"
-echo -e "$a IP $end -$bu ${IPSERV}.${MGVSERV} $end- $vr Conectado ✔$end"
-echo -e "$vr======================================== $end"
-echo -e "$vr    COMANDO EFETUADO COM SUCESSO... $end"
-echo -e "$vr======================================== $end"
-echo -e "${y}Retornando para o menu principal.
-⌛Por favor aguarde ⌛${endc}"
-sleep 3
-fi
-}
-# --------------
-# (15) MGV-SERVER
-gateway_filiais () {
-  logoNPDVs
-echo -e " ${c}ACESSO AO SERVIDOR ECD FILIAIS (CliPDVs)"
-painel
-echo -e "DIGITE A ${c}FAIXA DA FILIAL${end} ${br}QUE DESEJA O ACESSO AO GATEWAY: ${end}"
-read -p "$IPSERV." $read FAIXA
-echo -e "${bu}---------------------------------------------------${end}"
-echo -e "${y}⌛Aguarde enquanto testamos conexão com o terminal ⌛ ${end}"
-sleep 1
-if ! ping -c 2 ${IPSERV}.${FAIXA}.${GW} >> /dev/null ; then
-clear
-echo -e "$v======================================= $end"
-echo -e "$v       TERMINAL DESCONECTADO.           $end"
-echo -e "$v======================================= $end"
-echo -e "$v      _____ ____  ____   ___    _       $end"
-echo -e "$v     | ____|  _ \|  _ \ / _ \  | |      $end"
-echo -e "$v     |  _| | |_) | |_) | | | | | |      $end"
-echo -e "$v     | |___|  _ <|  _ <| |_| | |_|      $end"
-echo -e "$v     |_____|_| \_\_| \_\\____/  (_)     $end"
-echo && echo -e "$v======================================= $end"
-echo -e "$v======[ $br Status da requisição $ec $v]======= $end"
-echo -e "$a IP $end-$bu ${IPSERV}.${FAIXA}.${GW} $end- $v Sem conexão ✗$end" 
-echo -e "$v======================================= $end"
-echo -en "${y}Precione enter para retornar para o manu.${endc}"
-read input
-echo -e "$v=======================================$end" 
-else
-clear
-echo -e "$vr======================================== $end"
-echo -e "$vr         SERVIDOR CONECTADO.  $end "
-echo -e "$vr======================================== $end"
-rdesktop -g ${RES} -u ${GUSER} -p ${GPASS} ${IPSERV}.${FAIXA}.${GW}         
-echo -e "$vr=======[ $br Status da requisição $ec $vr]=======$end"
-echo -e "$a IP $end - $bu ${IPSERV}.${FAIXA}.${GW} $end - $vr Conectado$end"
-echo -e "$vr======================================== $end"
-echo -e "$vr    COMANDO EXECUTADO COM SUCESSO... $end"
-echo -e "$vr======================================== $end"
-echo -e "${y}Retornando para o menu principal.
-⌛Por favor aguarde ⌛${endc}"
-sleep 3
-fi
-}
-###TEST
-# (13) IMPORTA
-importa_pdvs () {
-  logoNPDVs
-echo -e " ${vr}ACESSO SERVIDOR IMPORTA (CliPDVs)
----------------------------------------------------${end}
-  ${br}Script externa.${end}
-${vr}--------------------------------------------------- ${end}"
-echo -e "${y}⌛Aguarde enquanto testamos conexão com o servidor ⌛${end}"
-sleep 1
-if ! ping -c 1 ${IMPORTA} >> /dev/null ; then
-clear
-echo -e "$v======================================= $end"
-echo -e "$v       SERVIDOR DESCONECTADO.           $end"
-echo -e "$v======================================= $end"
-echo -e "$v      _____ ____  ____   ___    _       $end"
-echo -e "$v     | ____|  _ \|  _ \ / _ \  | |      $end"
-echo -e "$v     |  _| | |_) | |_) | | | | | |      $end"
-echo -e "$v     | |___|  _ <|  _ <| |_| | |_|      $end"
-echo -e "$v     |_____|_| \_\_| \_\\____/  (_)     $end"
-echo && echo -e "$v======================================= $end"
-echo -e "$v======[ $br Status da requisição $ec $v]======= $end"
-echo -e "$a IP $end-$bu ${IMPORTA} $end- $v Sem conexão ✗$end" 
-echo -e "$v======================================= $end"
-echo -en "${y}Precione enter para retornar para o manu.${endc}"
-read input
-echo -e "$v=======================================$end" 
-else
-clear
-echo -e "$vr======================================== $end"
-echo -e "$vr         IMPORTA CONECTADO.  $end "
-echo -e "$vr         ${IMPORTA}  $end "
-echo -e "$vr======================================== $end"
-sshpass -p importa ssh -o "StrictHostKeyChecking no" importa@$IMPORTA "";
-echo -e "$vr=======[ $br Status da requisição $ec $vr]=======$end"
-echo -e "$a IP $end -$bu ${IMPORTA} $end- $vr Conectado ✔$end"
-echo -e "$vr======================================== $end"
-echo -e "$vr    COMANDO EFETUADO COM SUCESSO... $end"
-echo -e "$vr======================================== $end"
-echo -e "${y}Retornando para o menu principal.
-⌛Por favor aguarde ⌛${endc}"
-sleep 3
-fi
-}
-# --------------
-
 ###################################  (10) DELETAR PASTA /TMP #/mpos/maxipos/tmp ######################################
 dell_past_temp () {
   logoNPDVs
-echo -e " ${r}APAGAR ARQUIVOS TEMPORÁRIOS MAXIPOS (CliPDVs)"
+echo -e " ${r}APAGAR ARQUIVOS TEMPORÁRIOS MAXIPOS (NPDVs)"
 painel
 echo -e "DIGITE A ${y}FAIXA${end} ${r}REFERÊNTE A SUA FILIAL: ${end}"
 read -p "$IPSERV." $read FX
@@ -939,7 +796,7 @@ clear
 ##########
   clear
 logoNPDVs
-echo -e " ${r}APAGAR ARQUIVOS TEMPORÁRIOS MAXIPOS (CliPDVs)"
+echo -e " ${r}APAGAR ARQUIVOS TEMPORÁRIOS MAXIPOS (NPDVs)"
 loja
 echo -e "DIGITE O ${y}FINAL DO IP${end} ${r}QUE DESEJA APAGAR A PASTA TEMPORÁRIA: ${end}"
 read -p "$IPSERV.$FX." $read IP
@@ -985,7 +842,7 @@ sobre () {
   clear
   echo -e "
     ###########################################################
-    #                  Sobre o CliPDVs                        #
+    #                  Sobre o NPDVs                        #
     #     Script para automatização de tarefas CPDs Regional. #
     ###########################################################
     #    -- Op-System  :   Linux / Termux                     #
@@ -1005,7 +862,7 @@ sobre () {
      ${b}Reports${end}
     Você pode está acompanhando o desenvolvimento
     através de nossa página no GitHub:
-    ${u}${blue}https://github.com/nilsonlinux/CliPDVs${end}.
+    ${u}${blue}https://github.com/nilsonlinux/NPDVs${end}.
     Correção de bugs, implemento de código, e
     caso você pretenda utilizar e precisar mexer no código,
     favor manter os créditos."
@@ -1023,7 +880,7 @@ links () {
     ${bu}http://b2b.crednosso.com.br/garantia/loginGarantia.jsf
     ${bu}https://sitefconciliacao.softwareexpress.com.br/sitefweb/pages/inicial.zeus
     ${bu}https://telegram.org/dl/desktop/linux
-    ${bu}${blue}https://github.com/nilsonlinux/CliPDVs${end}"
+    ${bu}${blue}https://github.com/nilsonlinux/NPDVs${end}"
   echo -e " ${g}  -----------------------------------------${end}"
   echo && echo -en " ${yellow}Precione enter para retornar ao Menu.${endc}"
   read input
@@ -1032,7 +889,7 @@ links () {
 while :
 do
 logoNPDVs
-echo -e "${g}------[ MENU PRINCIPAL (CLIPDVS)]-------${end}
+echo -e "${g}------[ MENU PRINCIPAL (NPDVs)]-------${end}
 ${g}[ ${y}1 ${end}${g}]${end} ${vr} Reiniciar PDVs${end}
 ${g}[ ${y}2 ${end}${g}]${end} ${vr} Atualizar PDVs${end}
 ${g}[ ${y}3 ${end}${g}]${end} ${vr} Desligar PDVs${end}
@@ -1045,13 +902,10 @@ ${g}[ ${y}9 ${end}${g}]${end} ${vr} Atualizar imagem PDVs${end} ${r}(Todos)${end
 ${g}[ ${y}10${end}${g}]${end} ${vr} Deletar arquivos da pasta tmp${end}
 ${g}----------------------------------------- ${end}
 ${g}[ ${y}11${end}${g}]${end} ${vr} Gm core${end} ${y}(Desktop)${end}
-${g}[ ${y}12${end}${g}]${end} ${vr} MGV Server${end} ${y}(Desktop)${end}
-${g}[ ${y}13${end}${g}]${end} ${vr} Acesso Gateway Filiais${end} ${y}(Desktop)${end}
-${g}[ ${y}14${end}${g}]${end} ${vr} Acesso Importa PDVs${end}
 ${g}----------------------------------------- ${end}
-${g}[ ${y}15${end}${g}]${end} ${vr} Teste de conexão${end} ${vr}(PING)${end}
-${g}[ ${y}16${end}${g}]${end} ${vr} Teste de conexão${end} ${vr}(LINK-IP)${end}
-${g}[ ${y}17${end}${g}]${end} ${vr} Links úteis${end}
+${g}[ ${y}12${end}${g}]${end} ${vr} Teste de conexão${end} ${vr}(PING)${end}
+${g}[ ${y}13${end}${g}]${end} ${vr} Teste de conexão${end} ${vr}(LINK-IP)${end}
+${g}[ ${y}14${end}${g}]${end} ${vr} Links úteis${end}
 ${g}----------------------------------------- ${end}
 ${g}[ ${y}s ${end}${g}]${end} ${vr} Sobre${end}
 ${g}[ ${y}0 ${end}${g}]${end} ${vr} Sair${end}"
@@ -1070,9 +924,6 @@ case $option in
 9) atualizar_imagem_todos ;;
 10) dell_past_temp ;;
 11) gmcore ;;
-12) mgv_server ;;
-13) gateway_filiais ;;
-14) importa_pdvs ;;
 15) ping_test ;;
 16) ping_test_ip_link ;;
 17) links ;;
